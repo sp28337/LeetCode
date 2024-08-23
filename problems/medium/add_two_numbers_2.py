08a9e9f6eb6424264e4581dc52eae053f9ccd332
Pavel Tarakanov <pavel191094@icloud.com>
2024-08-23 14:03:47 +0300

    fix for readability;
    wrote unittests.

--- problems/medium/add_two_numbers_2.py
+++ problems/medium/add_two_numbers_2.py
@@ -2,9 +2,9 @@ from typing import Optional
 
 
 class ListNode(object):
-    def __init__(self, val=0, nxt=None):
+    def __init__(self, val=0, next=None):
         self.val = val
-        self.nxt = nxt
+        self.next = next
 
     def __str__(self):
         return str(self.val)
@@ -12,8 +12,7 @@ class ListNode(object):
 
 class AddTwoNumbers(object):
 
-    @staticmethod
-    def add_two_numbers(l1, l2) -> Optional[ListNode]:
+    def add_two_numbers(self, l1, l2) -> Optional[ListNode]:
         """
         Calculate values of two linked lists and return new linked list with sum of values
 
@@ -21,41 +20,24 @@ class AddTwoNumbers(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        # curr1, curr2 = l1, l2
-        # summ = curr1.val + curr2.val
-        # tmp = summ // 10
-        # l3 = ListNode(summ % 10 + tmp)
-        # curr3 = l3
-        #
-        # while (curr1 is not None) or (curr2 is not None):
-        #
-        #
-        #
-        #     curr1, curr2 = curr1.nxt, curr2.nxt
-        #     summ = curr1.val + curr2.val
-        #     curr3.nxt = ListNode(summ % 10 + tmp)
-        #     curr3 = curr3.nxt
-        #     tmp = summ // 10
-        # curr3.nxt = ListNode(tmp)
-        # return l3
-
-        curr1, curr2 = l1, l2
-        summ = curr1.val + curr2.val
-        tmp = summ // 10
-        l3 = ListNode(val=summ % 10, nxt=None)
+        tmp = 0
+        l3 = ListNode()
         curr3 = l3
 
-        while curr1 is not None or curr2 is not None:
-            curr1 = curr1.nxt if curr1 else None
-            curr2 = curr2.nxt if curr2 else None
-            if not any((curr1, curr2)):
-                break
-            x = curr1.val if curr1 else 0
-            y = curr2.val if curr2 else 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
             summ = x + y + tmp
-            curr3.nxt = ListNode(summ % 10)
-            curr3 = curr3.nxt
             tmp = summ // 10
-        if tmp > 0:
-            curr3.nxt = ListNode(tmp)
-        return l3
+
+            curr3.next = ListNode(summ % 10)
+            curr3 = curr3.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        if tmp:
+            curr3.next = ListNode(tmp)
+        return l3.next
