981ad8889b7f5773b85ec03dbc90cb0464e49b03
Pavel Tarakanov <pavel191094@icloud.com>
2024-08-23 14:50:38 +0300

    O(n^2) complexity

---
+++ problems/medium/longest_sub_without_rep_char_3.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    @classmethod
+    def length_of_longest_substring(cls, s: str) -> int:
+        """
+        Retern length of longest substring with unique symbols in <s>
+
+        :type s: str
+        :rtype: int
+        """
+
+        str_len = len(s)
+        sub = []
+        count = 0
+        for i in range(str_len):
+            if count > str_len - i:
+                break
+            for index in range(i, str_len):
+                if s[index] in sub:
+                    break
+                sub.append(s[index])
+            length = len(sub)
+            if length > count:
+                count = length
+            sub = []
+        return count
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    assert solution.length_of_longest_substring("abcabcbb") == 3
+    assert solution.length_of_longest_substring("bbbbb") == 1
+    assert solution.length_of_longest_substring("pwwkew") == 3
