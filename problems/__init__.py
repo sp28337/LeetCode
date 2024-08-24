d5fe0be353ab7ed1c9d150c3b13918eda2635ad7
Pavel Tarakanov <pavel191094@icloud.com>
2024-08-24 18:48:27 +0300

    add py-tests

--- problems/__init__.py
+++ problems/__init__.py
@@ -1,3 +1,3 @@
 from problems.easy import Solution
-from problems.medium import AddTwoNumbers, ListNode, LengthOfLongestSubstring
+from problems.medium import AddTwoNumbers, ListNode, LengthOfLongestSubstring, LongestPalindrome
 from problems.hard import FindMedianSortedArrays
