960a556757ced98d530b18cc685f58604cf3c30b
Pavel Tarakanov <pavel191094@icloud.com>
2024-08-27 12:20:02 +0300

    O(n) time complexity

---
+++ problems/easy/palindrome_number_9.py
@@ -0,0 +1,25 @@
+class IsPalindrome(object):
+    @staticmethod
+    def is_palindrome(x: int) -> bool:
+        """
+        Return True if x is palindrome number, else return False
+
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        length = len(x)
+        tail = length - 1
+        for head in range(length // 2):
+            if x[head] != x[tail]:
+                return False
+            tail -= 1
+        return True
+
+
+if __name__ == "__main__":
+    sol = IsPalindrome()
+    assert sol.is_palindrome(121) == True
+    assert sol.is_palindrome(1000021) == False
+    assert sol.is_palindrome(-121) == False
+    assert sol.is_palindrome(10) == False
