0a248c583a9cbda101b521e046887b24de39685c
Pavel Tarakanov <pavel191094@icloud.com>
2024-09-03 21:09:19 +0300

    recursive way, O(n) time

---
+++ problems/medium/swap_nodes_in_pairs_24.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+    def __eq__(self, other):
+        if not other:
+            return False
+        return self.val == other.val and self.next == other.next
+
+
+class Solution24(object):
+    def swap_pairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Recursively swap nodes in pairs inside linked list
+
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not isinstance(head, ListNode):
+            return None
+        if not head:
+            return
+        prev = head
+        curr = head.next
+
+        if curr:
+            curr.next = self.swap_pairs(curr.next)
+        else:
+            return head
+
+        curr.next, prev.next = prev, curr.next
+        return curr
+
+
+if __name__ == "__main__":
+    test = Solution24()
+
+    assert (test.swap_pairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4))))) ==
+            ListNode(2, ListNode(1, ListNode(4, ListNode(3)))))
+    assert (test.swap_pairs(ListNode(1)) == ListNode(1))
+    assert (test.swap_pairs(None) is None)
+    assert (test.swap_pairs("asdf") is None)
+    assert (test.swap_pairs(1254) is None)
+    assert (test.swap_pairs({"h": 777, "g": 97}) is None)
