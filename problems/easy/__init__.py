b8b9fa80e85d33fc5fe31edc4777b136a2719394
Pavel Tarakanov <pavel191094@icloud.com>
2024-09-10 19:36:02 +0300

    add pytests

--- problems/easy/__init__.py
+++ problems/easy/__init__.py
@@ -3,3 +3,4 @@ from problems.easy.palindrome_number_9 import IsPalindrome
 from problems.easy.balanced_binary_tree_110 import BalancedBinaryTree, Tree
 from problems.easy.min_depth_of_bst_111 import MinDepth
 from problems.easy.path_sum_112 import HasPathSum
+from problems.easy.pascal_triangle_118 import generate
